551cbea0f758c007ad79c367ab0a4440f0daf6c1
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-02-14 19:02:51 +0900

    add full post

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('docs');
@@ -11,6 +14,7 @@ export async function GET(context) {
       ...post.data,
       pubDate: `${post.data.date}`,
       link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
   });
 }
