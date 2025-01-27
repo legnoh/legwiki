38960a0374793b96ba2cc9262cd210717d5594a2
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-01-27 23:06:20 +0900

    start

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,7 +1,50 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config';
+import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
 import starlightMarkdoc from '@astrojs/starlight-markdoc';
 
 // https://docs.astro.build/en/guides/integrations-guide/markdoc/
 export default defineMarkdocConfig({
 	extends: [starlightMarkdoc()],
+	tags: {
+		oglinkcard: {
+			render: component('./src/components/Embed/OGLinkCard.astro'),
+			attributes: {
+				url: {
+					type: String,
+					required: true,
+				},
+				title: {
+					type: String,
+					required: false,
+				},
+				description: {
+					type: String,
+					required: false,
+				},
+				image: {
+					type: String,
+					required: false,
+				},
+				fixed: {
+					type: Boolean,
+					required: false,
+				},
+				url_amazon: {
+					type: String,
+					required: false,
+				},
+				url_yahoo: {
+					type: String,
+					required: false,
+				},
+				code_rakuten: {
+					type: String,
+					required: false,
+				},
+				keyword_rakuten: {
+					type: String,
+					required: false,
+				},
+			},
+		},
+	},
 });
