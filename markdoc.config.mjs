34fdbae9ac5ad72f39831d3cfab1de9894d5d783
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-01-27 23:09:41 +0900

    remove tab

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -3,48 +3,48 @@ import starlightMarkdoc from '@astrojs/starlight-markdoc';
 
 // https://docs.astro.build/en/guides/integrations-guide/markdoc/
 export default defineMarkdocConfig({
-	extends: [starlightMarkdoc()],
-	tags: {
-		oglinkcard: {
-			render: component('./src/components/Embed/OGLinkCard.astro'),
-			attributes: {
-				url: {
-					type: String,
-					required: true,
-				},
-				title: {
-					type: String,
-					required: false,
-				},
-				description: {
-					type: String,
-					required: false,
-				},
-				image: {
-					type: String,
-					required: false,
-				},
-				fixed: {
-					type: Boolean,
-					required: false,
-				},
-				url_amazon: {
-					type: String,
-					required: false,
-				},
-				url_yahoo: {
-					type: String,
-					required: false,
-				},
-				code_rakuten: {
-					type: String,
-					required: false,
-				},
-				keyword_rakuten: {
-					type: String,
-					required: false,
-				},
-			},
-		},
-	},
+  extends: [starlightMarkdoc()],
+  tags: {
+    oglinkcard: {
+      render: component('./src/components/Embed/OGLinkCard.astro'),
+      attributes: {
+        url: {
+          type: String,
+          required: true,
+        },
+        title: {
+          type: String,
+          required: false,
+        },
+        description: {
+          type: String,
+          required: false,
+        },
+        image: {
+          type: String,
+          required: false,
+        },
+        fixed: {
+          type: Boolean,
+          required: false,
+        },
+        url_amazon: {
+          type: String,
+          required: false,
+        },
+        url_yahoo: {
+          type: String,
+          required: false,
+        },
+        code_rakuten: {
+          type: String,
+          required: false,
+        },
+        keyword_rakuten: {
+          type: String,
+          required: false,
+        },
+      },
+    },
+  },
 });
